c42c12b3fc345225e4276d0a44afdb0d922333a8
joaogfernandes <joaoguilherme89@hotmail.com.br>
2024-09-23 22:29:08 -0300

    Update strategy value

--- components/Adsense/Adsense.tsx
+++ components/Adsense/Adsense.tsx
@@ -13,7 +13,7 @@ export default function Adsense({ adsenseKey }: props) {
       async
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${adsenseKey}`}
       crossOrigin="anonymous"
-      strategy="lazyOnload"
+      strategy="beforeInteractive"
     />
   );
 }
